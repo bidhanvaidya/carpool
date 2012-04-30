8384e4c94c82abb4dbf044359129cb4f3e4c33fa
Root <bidhan@bidhanUbuntu.(none)>
2012-04-30 22:36:08 +1000

    major changes hope noe errors

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,12 @@
 class ProfilesController < ApplicationController
 def index
-  @user= current_user.profile
+  @user= User.find(params[:user_id]).profile
 end
+def send_message
+   body=params[:body]
+    @user= User.find(params[:user_id]).profile
+   current_user.profile.send_message(@user, body, ("Message from "+current_user.name + " to "+ @user.first_name))
+    redirect_to posts_path
+end
+
 end
