5735bb0e0b884924252dd2b494a859f0b6596663
rails <rails@ubuntu.(none)>
2012-05-27 12:51:53 +1000

    Minor changes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 def index
 #@bidhan = FbGraph::User.fetch('Shrestha', :access_token => '363467483692946|rR0K1EePKHcxbIjSKN9e9meB2mA')
 #@a=FbGraph::User.me(User.find_by_provider_and_name('facebook', current_user.name).token).fetch
-
+@posts= Post.where("startdate>?", Date.yesterday).order("startdate").limit(3)
 end
 
 
