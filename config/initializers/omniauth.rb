898019b1723ead2c73441831c6ff5d19240b31a5
Bidhan <bidhanvaidya@gmail.com>
2012-11-26 15:32:21 +1100

    Update config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '363467483692946', 'ea2887ff9e64626cdfac2d40be6e47f3',:scope => 'user_work_history,email,user_education_history,user_interests,user_hometown,user_location,user_actions.music'
+  provider :facebook, 'app_ID', 'Secret token',:scope => 'user_work_history,email,user_education_history,user_interests,user_hometown,user_location,user_actions.music'
 end
 
